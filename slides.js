d2a2a6ed837144d4a6873e404c86e0c8159fd274
lxhoi <lxhoi.2k@gmail.com>
2025-08-27 00:03:31 +0700

    first commit

---
+++ slides.js
@@ -0,0 +1,34 @@
+const slides = [
+  {
+    title: "Desert Oasis Pool",
+    image: "/1.png",
+    url: "/projects/desert-oasis",
+  },
+  {
+    title: "Domed Sanctuary",
+    image: "/2.png",
+    url: "/projects/domed-sanctuary",
+  },
+  {
+    title: "Courtyard Retreat",
+    image: "/3.png",
+    url: "/projects/courtyard-retreat",
+  },
+  {
+    title: "Arched Corridor",
+    image: "/4.png",
+    url: "/projects/arched-corridor",
+  },
+  {
+    title: "Illuminated Grotto",
+    image: "/5.png",
+    url: "/projects/illuminated-grotto",
+  },
+  {
+    title: "Illuminated Grotto",
+    image: "/6.png",
+    url: "/projects/illuminated-grotto",
+  },
+];
+
+export default slides;
